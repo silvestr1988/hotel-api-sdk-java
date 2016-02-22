72513ea44d4614ae81dcbdc0db2226d26e4e199c
Verdoso <Verdoso@users.noreply.github.com>
2016-02-22 21:47:23 +0100

    Added new example about checking service status

---
+++ hotel-api-sdk-demo/src/main/java/com/hotelbeds/demo/simple/CheckingServiceStatus.java
@@ -0,0 +1,32 @@
+package com.hotelbeds.demo.simple;
+
+import com.hotelbeds.distribution.hotel_api_sdk.HotelApiClient;
+import com.hotelbeds.distribution.hotel_api_sdk.types.HotelSDKException;
+import com.hotelbeds.hotelapimodel.auto.messages.StatusRS;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class CheckingServiceStatus
+{
+
+  public static void main(String[] args)
+  {
+    // First initialise the client
+    HotelApiClient apiClient = new HotelApiClient();
+    apiClient.setReadTimeout(20000);
+    apiClient.init();
+    // Perform the request and browse the results
+    try
+    {
+      log.info("Requesting status...");
+      StatusRS statusRS = apiClient.status();
+      log.info("Status: {}", statusRS.getStatus());
+    }
+    catch (HotelSDKException e)
+    {
+      log.error("Error requesting status", e);
+    }
+  }
+
+}
