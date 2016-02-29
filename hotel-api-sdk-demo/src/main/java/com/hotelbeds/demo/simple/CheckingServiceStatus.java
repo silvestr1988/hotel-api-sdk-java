43ad16f4e12b1facf786f3813d4fa8e6d87148d7
Daniel Lopez <dalopez@hotelbeds.com>
2016-02-29 09:03:53 +0100

    Properly formatted demo for status checking

--- hotel-api-sdk-demo/src/main/java/com/hotelbeds/demo/simple/CheckingServiceStatus.java
+++ hotel-api-sdk-demo/src/main/java/com/hotelbeds/demo/simple/CheckingServiceStatus.java
@@ -1,5 +1,28 @@
 package com.hotelbeds.demo.simple;
 
+/*
+ * #%L
+ * HotelAPI SDK Demo
+ * %%
+ * Copyright (C) 2015 - 2016 HOTELBEDS TECHNOLOGY, S.L.U.
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2.1 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ * #L%
+ */
+
+
 import com.hotelbeds.distribution.hotel_api_sdk.HotelApiClient;
 import com.hotelbeds.distribution.hotel_api_sdk.types.HotelSDKException;
 import com.hotelbeds.hotelapimodel.auto.messages.StatusRS;
@@ -7,26 +30,21 @@ import com.hotelbeds.hotelapimodel.auto.messages.StatusRS;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class CheckingServiceStatus
-{
+public class CheckingServiceStatus {
 
-  public static void main(String[] args)
-  {
-    // First initialise the client
-    HotelApiClient apiClient = new HotelApiClient();
-    apiClient.setReadTimeout(20000);
-    apiClient.init();
-    // Perform the request and browse the results
-    try
-    {
-      log.info("Requesting status...");
-      StatusRS statusRS = apiClient.status();
-      log.info("Status: {}", statusRS.getStatus());
+    public static void main(String[] args) {
+        // First initialise the client
+        HotelApiClient apiClient = new HotelApiClient();
+        apiClient.setReadTimeout(20000);
+        apiClient.init();
+        // Perform the request and browse the results
+        try {
+            log.info("Requesting status...");
+            StatusRS statusRS = apiClient.status();
+            log.info("Status: {}", statusRS.getStatus());
+        } catch (HotelSDKException e) {
+            log.error("Error requesting status", e);
+        }
     }
-    catch (HotelSDKException e)
-    {
-      log.error("Error requesting status", e);
-    }
-  }
 
 }
